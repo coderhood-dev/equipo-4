1d2f7345d539fbf01ed7dbb5395f82a9038ebacf
barrod-code <rbarros418@gmail.com>
2020-12-05 17:15:03 -0300

    added react-query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,67 +1,56 @@
-import React from 'react';
-import {
-  Link
-} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import {
-  Flex, 
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Heading,
-  Button,
-    
-
-} from '@chakra-ui/react'
-
+	Flex,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Heading,
+	Button,
+} from "@chakra-ui/react";
 
 const Header = (props) => (
-<Flex 
-  bg="#065666"
-  color="#C4F1F9"
-  h="4rem"
-  pl="4rem"
-  pr="4rem"
-  w="100%"  
-  justify="space-between"
-  alignItems="center"
-  >
-  <Heading as="h1" >{props.pagename}</Heading>
-  <Menu>
-    <Flex ml="2rem">
-        <MenuButton  
-        as={Button}
-        color="#C4F1F9"
-        _hover={{bg:"#0987A0"}}
-        _expanded={{ bg: "#0987A0" }}
-        variant="ghost"
-        w="2rem"
-        borderRadius="8px"
-        size="lg"
-
-        >
-        Menu
-        
-        </MenuButton>
-        <MenuList 
-        bg="#065666"
-        _expanded={{ bg: "#0987A0" }}
-        >
-          <MenuItem _hover={{ bg: "#0987A0" }}>
-            <Link  to="/">Home</Link>
-          </MenuItem>
-          <MenuItem _hover={{ bg: "#0987A0" }}>
-            <Link to="/about">About</Link>
-          </MenuItem>
-          <MenuItem _hover={{ bg: "#0987A0" }}>
-            <Link to="/profile">Profile</Link>
-          </MenuItem>
-        </MenuList>
-    </Flex>    
-  </Menu>
-</Flex>
+	<Flex
+		bg="#065666"
+		color="#C4F1F9"
+		h="4rem"
+		pl="4rem"
+		pr="4rem"
+		w="100%"
+		justify="space-between"
+		alignItems="center"
+	>
+		<Heading as="h1">{props.pagename}</Heading>
+		<Menu>
+			<Flex ml="2rem">
+				<MenuButton
+					as={Button}
+					color="#C4F1F9"
+					_hover={{ bg: "#0987A0" }}
+					_expanded={{ bg: "#0987A0" }}
+					variant="ghost"
+					w="2rem"
+					borderRadius="8px"
+					size="lg"
+				>
+					Menu
+				</MenuButton>
+				<MenuList bg="#065666" _expanded={{ bg: "#0987A0" }}>
+					<Link to="/">
+						<MenuItem _hover={{ bg: "#0987A0" }}>Home</MenuItem>
+					</Link>
+					<Link to="/about">
+						<MenuItem _hover={{ bg: "#0987A0" }}>About</MenuItem>
+					</Link>
+					<Link to="/profile">
+						<MenuItem _hover={{ bg: "#0987A0" }}>Profile</MenuItem>
+					</Link>
+				</MenuList>
+			</Flex>
+		</Menu>
+	</Flex>
 );
-
 
 export default Header;
