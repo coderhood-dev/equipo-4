afb5823460aaa8b0e4a1ff23e676f666694a744c
Barrod-Code <rbarros418@gmail.com>
2021-01-13 23:08:56 -0300

    Consider error handling fixed for now

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -8,16 +8,11 @@ function SearchResults({ queryURL }) {
   const { isLoading, error, data } = useQuery(
     ['foodData', queryURL],
     async () => {
-      try {
-        const response = await fetch(queryURL);
-        if (response.ok) {
-          return response.json();
-        }
-        throw Error({ status: response.status, message: response.statusText });
-      } catch (e) {
-        console.log('ERROR', { e });
-        throw Error(e);
+      const response = await fetch(queryURL);
+      if (response.ok) {
+        return response.json();
       }
+      throw Error(`code ${response.status}`);
     },
     { refetchOnWindowFocus: false }
   );
@@ -25,8 +20,7 @@ function SearchResults({ queryURL }) {
   if (isLoading) return 'Loading...';
 
   if (error) {
-    console.log(error);
-    return `An error has occurred: ${error}`;
+    return `An error has occurred: ${error.message}`;
   }
   return (
     <Box>
